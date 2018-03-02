630f820688d0a4f151be34c853ef3192e295e156
orazbay <orazbay.n.n@gmail.com>
2018-03-02 15:21:59 +0600

    correct login

--- parent/client/front/ts/app/app.component.ts
+++ parent/client/front/ts/app/app.component.ts
@@ -5,7 +5,32 @@ import "rxjs/add/operator/toPromise";
 
 @Component({
     selector:"blog_app",
-    template:`<h1> text is {{text}}</h1>`
+    template:`<h1> text is {{text}}</h1>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <!-- Tabs Titles -->
+            <h2 class="active"> Sign In </h2>
+            <h2 class="inactive underlineHover">Sign Up </h2>
+
+            <!-- Icon -->
+            <div class="fadeIn first">
+                <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+            </div>
+
+            <!-- Login Form -->
+            <form>
+                <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+                <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+                <input type="submit" class="fadeIn fourth" value="Log In">
+            </form>
+
+            <!-- Remind Passowrd -->
+            <div id="formFooter">
+                <a class="underlineHover" href="#">Forgot Password?</a>
+            </div>
+
+        </div>
+    </div>`
 })
 export class AppComponent implements OnInit {
     private text:string;
